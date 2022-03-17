c1591ba8d9db8706b5a2a7ef0a81959a4be776ee
mmeising <max_meisinger@hotmail.com>
2022-03-17 03:17:28 +0100

    added struct freeing and cycling through commands

--- src/pipex.c
+++ src/pipex.c
@@ -6,7 +6,7 @@
 /*   By: mmeising <mmeising@student.42heilbronn.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/09 18:22:43 by mmeising          #+#    #+#             */
-/*   Updated: 2022/03/12 00:39:58 by mmeising         ###   ########.fr       */
+/*   Updated: 2022/03/17 02:31:33 by mmeising         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -62,6 +62,8 @@ int	get_cmd_count(t_cmd	*cmd_list)
 	// at end: while (wait(NULL) != -1 || errno != ECHILD);
 }
 
+//	piping has to set the redirs before redirections function does it
+//	because redirections overwrite the pipe redirection
 void	redirections(t_cmd *cmd)
 {
 	t_files	*red;
@@ -69,7 +71,7 @@ void	redirections(t_cmd *cmd)
 	red = cmd->redirs;
 	while (red)
 	{
-		if (cmd->fd_in && (red->type == IN || red->type == HEREDOC))
+		if (cmd->fd_in  > 2 && (red->type == IN || red->type == HEREDOC))
 		{
 			close(cmd->fd_in);
 			cmd->fd_in = 0;
@@ -92,17 +94,42 @@ void	redirections(t_cmd *cmd)
 }
 
 //	close write-end of pipe and wait for child
+//	maybe don't wait for the child, open file descriptors take care of waiting
 void	parent(int pid, int *fd)
 {
 	close(fd[1]);
-	waitpid(pid, NULL, 0);
+	close(fd[0]);
+	// waitpid(pid, NULL, 0);
 }
 
-//	go through all redirections, execve
+//	go through all redirections, dup the redirs into stdin and stdout, execve
 void	child(t_cmd *cmds, char **envp)
 {
 	if (cmds->redirs)
 		redirections(cmds);
+}
+
+void	free_cmd_struct(t_cmd **cmd_addr)
+{
+	while ((*cmd_addr)->redirs)
+	{
+		free((*cmd_addr)->redirs->file);
+		(*cmd_addr)->redirs = (*cmd_addr)->redirs->next;
+	}
+	while (*((*cmd_addr)->cmd))//does this work like this? does it point to one string in the 2d array?
+	{
+		free(*((*cmd_addr)->cmd));
+		*((*cmd_addr)->cmd) += sizeof(char *);//does this make the pointer point to the next string?
+	}
+}
+
+t_cmd	*cmds_free_and_next(t_cmd **cmds_addr)
+{
+	t_cmd	*temp;
+
+	temp = *cmds_addr;
+	*cmds_addr = (*cmds_addr)->next;
+	free_cmd_struct(&temp);
 }
 
 int	piping(t_cmd *cmds, char **envp)
@@ -125,6 +152,8 @@ int	piping(t_cmd *cmds, char **envp)
 	// }
 	// free(paths);
 	// printf("cmd_count: %i\n", cmd_count);
+
+	// here_doc_prep(cmds);
 	while (cmds)
 	{
 		if (pipe(fd) < 0)
@@ -133,12 +162,13 @@ int	piping(t_cmd *cmds, char **envp)
 		if (pid < 0)
 			error(FORK);
 		else if (pid == 0)
-			exit(0);//child
+			child(cmds, envp);//child
 		else
 		{
 			parent(pid, fd);
-			exit(4);//parent
+			// exit(4);//parent
 		}
+		cmds = cmds_free_and_next(&cmds);
 	}
 	return (0);
 }
